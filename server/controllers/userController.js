cd275a7b55ff3d3e0f52249da1b825912bb10ee7
Dima Holovachenko <dmytro.holovachenko@infotech.gov.ua>
2022-11-21 23:10:47 +0200

    refactor api

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,60 +1,53 @@
-const jwt = require("jsonwebtoken")
-const bcrypt = require("bcrypt")
-const ApiError = require("../error/ApiError")
-const {User} = require('../models/models')
-const jwtService = require("../services/jwtService")
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const ApiError = require("../error/ApiError");
+const { User } = require("../models/models");
+const jwtService = require("../services/jwtService");
+const userService = require("../services/userService");
 
 class UserController {
-
-  async registration(req, res, next){
+  async registration(req, res, next) {
     try {
-      const {email, username, password, role} = req.body
-  
-      const candidate = await User.findOne({where: {email}});
-      if (candidate) {
-        return next(ApiError.badRequestError(`Користувач з email ${email} вже створений`));
-      }
-  
-      const passHash = bcrypt.hashSync(password, 5)
-      const user = await User.create({email, username, password: passHash});
-  
+      const { email, username, password } = req.body;
+
+      const user = await userService.createUser(email, username, password);
+
       const payload = {
         id: user.id,
         email,
-        username
-      }
+        username,
+      };
       const token = jwtService.generateToken(payload);
-  
+
       return res.json(token);
     } catch (error) {
-      return next(ApiError.badRequestError(error.message))
+      return next(ApiError.badRequestError(error.message));
     }
   }
 
-  async login(req, res, next){
+  async login(req, res, next) {
     try {
-      const { email, password } = req.body
+      const { email, password } = req.body;
 
-      const user = await User.findOne({ where: { email } });
-      if (!user) {
-        return next(ApiError.badRequestError(`Користувача не знайдено`));
-      }
+      const user = await userService.getUserByEmail(email);
 
       const isPassValid = bcrypt.compareSync(password, user.password);
       if (!isPassValid) {
-        return next(ApiError.badRequestError(`Пароль користувача неправильний`));
+        return next(
+          ApiError.badRequestError(`Пароль користувача неправильний`)
+        );
       }
 
       const payload = {
         id: user.id,
         email: user.email,
         username: user.username,
-      }
-      const token = jwtService.generateToken(payload);  
+      };
+      const token = jwtService.generateToken(payload);
 
       return res.json(token);
     } catch (error) {
-      return next(ApiError.badRequestError(error.message))
+      return next(ApiError.badRequestError(error.message));
     }
   }
 
@@ -63,57 +56,50 @@ class UserController {
       const payload = {
         id: req.user.id,
         email: req.user.email,
-        username: req.user.username
-      }
+        username: req.user.username,
+      };
       const token = jwtService.generateToken(payload);
 
       return res.json(token);
     } catch (error) {
-      next(ApiError.internalError('Непердбачувана помилка - ' + error));
+      next(ApiError.internalError("Непердбачувана помилка - " + error));
     }
   }
 
   async changePassword(req, res, next) {
     try {
-      const { password, newPassword } = req.body
-      const userId = req.user.id
-      const user = await User.findByPk(userId);
-      if (!user) {
-        return next(ApiError.badRequestError(`Користувача не знайдено`));
-      }
+      const { password, newPassword } = req.body;
+      const userId = req.user.id;
+      const user = await userService.getUserById(userId);
 
       const isPassValid = bcrypt.compareSync(password, user.password);
       if (!isPassValid) {
-        return next(ApiError.badRequestError(`Пароль користувача неправильний`));
+        return next(
+          ApiError.badRequestError(`Пароль користувача неправильний`)
+        );
       }
 
       const newPassHash = bcrypt.hashSync(newPassword, 5);
-      user.password = newPassHash;
-      await user.save();
+      await userService.changeUserPassword(userId, newPassHash);
 
-      return res.json({message: 'Пароль успішно змінено'});
+      return res.json({ message: "Пароль успішно змінено" });
     } catch (error) {
-      return next(ApiError.badRequestError(error.message))
+      return next(ApiError.badRequestError(error.message));
     }
   }
 
   async changeUsername(req, res, next) {
     try {
-      const { username } = req.body
-      const userId = req.user.id
-      const user = await User.findByPk(userId);
-      if (!user) {
-        return next(ApiError.badRequestError(`Користувача не знайдено`));
-      }
+      const { username } = req.body;
+      const userId = req.user.id;
 
-      user.username = username;
-      await user.save();
+      await userService.changeUsername(userId, username);
 
-      return res.json({ message: 'Пароль успішно змінено' });
+      return res.json({ message: "Пароль успішно змінено" });
     } catch (error) {
-      return next(ApiError.badRequestError(error.message))
+      return next(ApiError.badRequestError(error.message));
     }
   }
 }
 
-module.exports = new UserController()
+module.exports = new UserController();
